872a434f15f14ee1d3ed39e3ea372bd2a3b72a6f
Kanchan Sharma <ksrikhi123@gmail.com>
2020-08-26 23:33:21 +0530

    Updates

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from './containers/layout';
-
-function App() {
-  return <Layout/>
-}
-
-export default App;
--- src/component/section.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Section = styled.section`
-  padding: 40px 0;
-  border-top: 1px solid #e3e3e3;
-`;
-
-export default Section;
