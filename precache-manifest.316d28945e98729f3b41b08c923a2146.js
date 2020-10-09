9c5b6766865dd32ae2890b121d584f3d02757809
Kanchan Sharma <ksrikhi123@gmail.com>
2020-10-10 02:45:22 +0530

    Updates

---
+++ precache-manifest.316d28945e98729f3b41b08c923a2146.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5878890aa317c76f169ba709a9a0e9",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "cc2f6de10e41be8e0d02",
+    "url": "/profile/static/css/main.883dc074.chunk.css"
+  },
+  {
+    "revision": "e98a62637c7b26f1e5b1",
+    "url": "/profile/static/js/2.82b7aa21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile/static/js/2.82b7aa21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc2f6de10e41be8e0d02",
+    "url": "/profile/static/js/main.e2b9e884.chunk.js"
+  },
+  {
+    "revision": "c39ac03537bb71245439",
+    "url": "/profile/static/js/runtime-main.8a6bdc07.js"
+  },
+  {
+    "revision": "9c020242b01adc9fef0b99a34ea1a61e",
+    "url": "/profile/static/media/img.9c020242.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.524d12a1f1c33fa782716292212adb85.js"
+  "/profile/precache-manifest.316d28945e98729f3b41b08c923a2146.js"
 );
 
 self.addEventListener('message', (event) => {
